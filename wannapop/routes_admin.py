8279397d96651129f91fb09b726c16a384534846
mriuse <mriuse@fp.insjoaquimmir.cat>
2023-12-01 21:09:21 +0100

    Inici menú admin prod.

--- wannapop/routes_admin.py
+++ wannapop/routes_admin.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, current_app
 from flask_login import current_user, login_required
-from .models import User
+from .models import User, Product
 from .helper_role import require_admin_moderator, require_admin_role
 from . import db_manager as db
 
@@ -22,3 +22,17 @@ def admin_users():
     current_app.logger.debug('Loading user list...')
     users = db.session.query(User).all()
     return render_template('users_list.html', users=users)
+
+@admin_bp.route('/admin/products')
+@login_required
+@require_admin_role.require(http_exception=403)
+def admin_products():
+    current_app.logger.debug('Loading product list...')
+    products = db.session.query(Product).all()
+    return render_template('products_list.html', products=products)
+
+@admin_bp.route('/admin/products/<int:product_id>/ban')
+@login_required
+@require_admin_role.require(http_exception=403)
+def ban_product(product_id):
+    return 
