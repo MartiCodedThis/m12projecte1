38d87ac146c0ae51171c1d48028f14c8ae92a279
mriuse <mriuse@fp.insjoaquimmir.cat>
2024-01-19 16:30:52 +0100

    Mixins base

--- wannapop/models.py
+++ wannapop/models.py
@@ -1,8 +1,9 @@
 from . import db_manager as db
 from sqlalchemy.sql import func
 from flask_login import UserMixin
+from .mixins import BaseMixin, SerializableMixin
 
-class Product(db.Model):
+class Product(db.Model, BaseMixin, SerializableMixin):
     __tablename__ = "products"
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
@@ -14,13 +15,13 @@ class Product(db.Model):
     created = db.Column(db.DateTime, server_default=func.now())
     updated = db.Column(db.DateTime, server_default=func.now(), onupdate=func.now())
 
-class Category(db.Model):
+class Category(db.Model, BaseMixin, SerializableMixin):
     __tablename__ = "categories"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
     slug = db.Column(db.String, nullable=False)
 
-class User(db.Model, UserMixin):
+class User(db.Model, BaseMixin, SerializableMixin, UserMixin):
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
@@ -29,17 +30,19 @@ class User(db.Model, UserMixin):
     role = db.Column(db.String, nullable = False)
     email_token = db.Column(db.String)
     verified = db.Column(db.Integer)
+
+    exclude_attr = ['password']
     
     def get_id(self):
         return self.email
     
-class BlockedUser(db.Model):
+class BlockedUser(db.Model, BaseMixin, SerializableMixin):
     __tablename__ = "blocked_users"
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), primary_key=True)
     message = db.Column(db.String, nullable=False)
     created = db.Column(db.DateTime, server_default=func.now())
 
-class BannedProduct(db.Model):
+class BannedProduct(db.Model, BaseMixin, SerializableMixin):
     __tablename__ = "banned_products"
     product_id = db.Column(db.Integer, db.ForeignKey('products.id'), primary_key=True)
     reason = db.Column(db.String, nullable=False)
