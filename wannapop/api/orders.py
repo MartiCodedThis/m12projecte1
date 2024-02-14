cca66dcdfea78741da8e1c2e505d9f8f5b0ebf9a
mriuse <mriuse@fp.insjoaquimmir.cat>
2024-02-14 20:10:59 +0100

    WIP changes to token_auth

--- wannapop/api/orders.py
+++ wannapop/api/orders.py
@@ -3,39 +3,41 @@ from .errors import not_found, bad_request
 from ..models import Order, ConfirmedOrder
 from .helper_auth import token_auth
 from .helper_json import json_response, json_request
-from .helper_auth import basic_auth
+from .helper_auth import token_auth
 from flask import current_app
 
 
 @api_bp.route('/orders', methods = ['POST'])
-@basic_auth.login_required
+@token_auth.login_required
 def api_order_add():
-    try:
-        user_id = token_auth.current_user().id
-        data = json_request(['product_id', 'buyer_id', 'offer', 'created'], False)
-        order = Order.create(**data, buyer_id=user_id)
-    except Exception as e:
-            current_app.logger.debug(e)
-            return bad_request(str(e))
-    else:
-        return json_response(order.to_dict())
+    user_id = token_auth.current_user().id
+    data = json_request(['product_id', 'buyer_id', 'offer', 'created'], False)
+    order = Order.create(**data, buyer_id=user_id)
+    return json_response(order.to_dict())
 
 @api_bp.route('/orders/<int:order_id>', methods = ['PUT'])
+@token_auth.login_required
 def api_order_edit(order_id):
-    order = Order.get(order_id)
-    if order:
-        try:
-            data = json_request(['id', 'product_id', 'buyer_id', 'offer', 'created'], False)
-        except Exception as e:
-            current_app.logger.debug(e)
-            return bad_request(str(e))
+    confirmed = ConfirmedOrder.get(order_id)
+    if not confirmed:
+        order = Order.get(order_id)
+        if order:
+            try:
+                user_id = token_auth.current_user().id
+                data = json_request(['product_id', 'buyer_id', 'offer', 'created'], False)
+            except Exception as e:
+                current_app.logger.debug(e)
+                return bad_request(str(e))
+            else:
+                order.update(**data, buyer_id=user_id)
+                current_app.logger.debug("UPDATED order: {}".format(order.to_dict()))
+                return json_response(order.to_dict())
         else:
-            order.update(**data)
-            current_app.logger.debug("UPDATED order: {}".format(order.to_dict()))
-            return json_response(order.to_dict())
+            current_app.logger.debug("Order {} not found".format(order_id))
+            return not_found("Order not found")
     else:
-        current_app.logger.debug("Order {} not found".format(order_id))
-        return not_found("Order not found")
+        current_app.logger.debug("Order {} already confirmed".format(order_id))
+        return not_found("Order already confirmed")
 
 @api_bp.route('/orders/<int:order_id>', methods = ['DELETE'])
 def api_order_delete(order_id):
