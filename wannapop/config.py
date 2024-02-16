407d1c341579afaa5cc58a5acffc29a6b0b54408
mriuse <mriuse@fp.insjoaquimmir.cat>
2024-02-16 16:43:18 +0100

    Postman API json included

--- wannapop/config.py
+++ wannapop/config.py
@@ -9,8 +9,11 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 LOG_LEVEL = environ.get('LOG_LEVEL', 'DEBUG').upper()
 
 SECRET_KEY = "Valor aleatori molt llarg i super secret"
+# SQLite #
 # SQLALCHEMY_DATABASE_URI = "sqlite:///"+os.path.join(basedir, "../database.db")
 
+# MySQL #
+# SQLALCHEMY_DATABASE_URI = "mysql+pymysql://2dd06:uttVEcUemINNCX47@37.27.3.70:3306/2dd06_my"
 SQLALCHEMY_DATABASE_URI = "postgresql://2dd06:uttVEcUemINNCX47@37.27.3.70:5432/2dd06_pg"
 SQLALCHEMY_ECHO = False 
 
